667f5912597f925fedb5072758e7fc1f82dbb5a5
goyalk130 <goyalkaran130@gmail.com>
2022-03-27 16:26:24 +0530

    .

--- src/components/HotAccessories.jsx
+++ src/components/HotAccessories.jsx
@@ -16,6 +16,7 @@ export default function HotAccessories({
                 <HotItemCard key={item.image} name={item.name} price={item.price} image={item.image} index={index} />
 
             ))}
+            <HotItemCard image="https://i01.appmifile.com/webfile/globalimg/in/cms/593181BE-7DB8-15F5-E907-506427B8C9F4.jpg" />
         </div>
     </div>
   )
